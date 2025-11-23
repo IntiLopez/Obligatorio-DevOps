1c185c8fad30e7a7f8dc6f0f1db9f187ff1f81cc
Inti Lopez <intilopez97@gmail.com>
2025-11-23 20:11:36 +0000

    semi final

--- Ejercicio_1/ejercicio1_obligatorio.sh
+++ Ejercicio_1/ejercicio1_obligatorio.sh
@@ -29,7 +29,7 @@ while [ $# -gt 1 ];do	#Recorremos los parametros
 				echo "Error, Falta agregar la contraseña luego del parametro" >&2
 				exit 2
 			fi
-			contra="-p $2"	#Guardo el -p y la contraseña para tirar la variable entera al useradd
+			contra="-p \"$2\""	#Guardo el -p y la contraseña para tirar la variable entera al useradd
 			shift 2
 			;;
 		-*)	#COMENTAR
@@ -54,10 +54,9 @@ fi
 #Esta bien el archivo??
 #La esctructura del archivo es incorrecta testeando
 cont=0
-contusu=0
 for linea in $(cat $archivo);do
         cont=$((cont+1)) #Doble parentesis para que haga una suma posta y no strings turbios
-        if ! [[ "$linea" =~ ^[a-z_][a-z0-9_-]*:[^:].*:+/.*:(SI|NO):/bin/.*$ ]];then
+        if ! [[ "$linea" =~ ^[a-z_][a-z0-9_-]*:[^:].*:+/.*:(SI|NO):[^:].*$ ]];then
 		#Quedo largo pero esto verifica correctamente toda la linea completa y verifica
 		#que el campo usuario no este vacio
                 #https://unix.stackexchange.com/questions/287077/why-cant-linux-usernames-begin-with-numbers
@@ -75,7 +74,6 @@ for linea in $(cat $archivo);do
 	fi
 	#----------------------------------------------
 	usuario=$(echo "$linea" | cut -d":" -f1)
-	
 	comentario="$(echo "$linea" | cut -d":" -f2)" 2> /dev/null #Si el comentario esta vacio que el cut no moleste
 	home=$(echo "$linea" | cut -d":" -f3)
 	crear=$(echo "$linea" | cut -d":" -f4)
@@ -87,36 +85,75 @@ for linea in $(cat $archivo);do
         fi
 	#if [ -z "$usuario" ];then #Esto ya lo controlo en el Regex
 	#---------------Variables de testeo---------
-	echo $usuario
-	echo $comentario
-	echo $creard
-	echo $home
-	echo $gsh
+	#echo $usuario
+	#echo $comentario
+	#echo $creard
+	#echo $home
+	#echo $gsh
 	#-------------------PENDIENTES------------------------
 	#Hacer dos versiones del useradd una con esto que esta debajo y otra con las opciones por defecto
 	#Si esta con las opciones por defecto que hay que separar con su parametro "-"
 	#tambien hay que cambiar la linea del Regex ya que no aceptaria la bash vacia para que deje por defecto
 	#revisar si el Regex filtra algo más
-	useradd $creard -d $home -c "$comentario" -s "$shell" $usuario #2> /dev/null
+	#----------------------------SOLUCION PARA EL CULO--------------------
+	#Aca va un -z or cada variable negado si es que no esta ninguna vacia sino que entre aca
+	#for r in $(echo -e $home\n$comentario\n$shell\ | cat);do #saco las variables y las coloco en el archivo pipe para digierirlas
+	#	if [ -z $r ];then #Si esta vacia 
+	#		
+	#	fi
+	#done
+	# Otra solucion que no llego a nada
+	#
+	#if ! [ -z $comentario] || [  -z $home] || [ -z $gsh];then #Si ninguna esta vacia ejecute
+	#	useradd $creard -d $home -c "$comentario" -s "$shell" $usuario #2> /dev/null
+	#else
 
+	#	useradd  $creard -d $home -c "$comentario" -s "$shell" $usuario
+	#fi#salgo
+	#----------------------CAMPOS POR DEFECTO---------------------------------------
+	if  ! [ -z $comentario ];then
+		armocomand=" -c \"$comentario\""
+	fi
+	if ! [  -z $home ];then
+		armocomand=($armocomand + " -d \"$home\" ")  
+	fi
+	if ! [ -z $gsh ];then
+		armocomand=($armocomand + " -s \"$gsh\" ") 
+	fi
+	#	echo "No hay vacias" 2> /dev/null #Comentario solamente para tirar el else
+	if [ -z $armocomand ];then
+		useradd  \"$usuario\" $creard $contra
+	else
+		useradd \"$usuario\"$armocomand$creard $contra 
+		echo "useradd \"$usuario\"$armocomand$creard $contra"
+	fi
+
+	#-------------------------------------------------------------
 	if [[ "$desplegar" = "1" ]];then
 		usuariocreado=$(cat /etc/passwd | cut -d":" -f1 | grep $usuario)
-		if [ -z "$usuario"  ];then
+		if [ -z "$usuariocreado"  ];then
 			errorfatal="true"
 		else
+
 			echo Usuario $usuario creado con éxito con datos indicados:
 			echo "	Comentario: $comentario"
 			echo "	Dir home: $home"
 			echo "	Asegurado existencia de directorio home: $crear"
 			echo "	Shell por defecto: $gsh"
+			echo corresponde a la linea $cont
 			#----------------PENDIENTES 2-------------------------------
 			#Estas variables si estan por defecto tienen que venir con el valor que dice la letra
 			#La idea es que si estan por defecto "vacias" que directamente le agregue a todas el
 			#valor
 		fi
-		if [[ "$errorfatal" = "true"]];then
+		if [[ "$errorfatal" = "true" ]];then
 			echo ATENCION: el usuario $usuario de la linea $cont no pudo ser creado
 		fi
-	errorfatal="falso" #Seteo la variable preparandola para el siguente recorrido
 	fi
+	#Reseteo de variables por ciclo
+	errorfatal="falso" #Seteo la variable preparandola para el siguente recorrido
+	c="0"
+	h="0"
+	g="0"
+	#Fin dle recorrido
 done #Fin del for de recorrida de linea a linea
